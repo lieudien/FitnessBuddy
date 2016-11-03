bb93a04f44db7a5b0eb5112ef9977c768695ce15
Danny Lieu <young_uno1080@yahoo.com>
2016-11-03 12:24:58 -0700

    Add TabLayout

---
+++ app/src/main/java/com/comp7081/project/fitnessbuddy/Fragments/PageFragment.java
@@ -0,0 +1,38 @@
+package com.comp7081.project.fitnessbuddy.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.design.R;
+
+/**
+ * Created by danny on 2016-11-03.
+ */
+
+public class PageFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static PageFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment_page, container, false);
+
+        return view;
+    }
+}
